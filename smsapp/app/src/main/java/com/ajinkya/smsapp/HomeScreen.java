0263c02515fa082e582c6db0f2e7f5609cf78ff3
Ajinkya Aher <ajinkya.aher@esds.co.in>
2024-01-30 16:45:12 +0530

    Added redirection on the local notifications.

---
+++ smsapp/app/src/main/java/com/ajinkya/smsapp/HomeScreen.java
@@ -0,0 +1,55 @@
+package com.ajinkya.smsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.Calendar;
+
+public class HomeScreen extends AppCompatActivity {
+    Button buttonNotification, buttonMessage, buttonPeriodicNotifications;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        buttonMessage = findViewById(R.id.button2);
+        buttonNotification = findViewById(R.id.button);
+        buttonPeriodicNotifications = findViewById(R.id.button3);
+        try {/// Code for periodic Notifications.
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, 15);
+            calendar.set(Calendar.MINUTE, 57);
+            calendar.set(Calendar.SECOND, 0);
+
+            Intent calendarIntent = new Intent(getApplicationContext(), NotificationReceiver.class);
+            PendingIntent pendingIntent;
+            pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 100, calendarIntent, PendingIntent.FLAG_MUTABLE);
+            AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+            manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+
+        } catch (Exception e) {
+            throw e;
+        }
+        buttonMessage.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        buttonNotification.setOnClickListener(v -> {
+            NotificationReceiver notificationReceiver = new NotificationReceiver();
+            notificationReceiver.onReceive(this, new Intent(this, MainActivity.class));
+        });
+
+    }
+}
---
+++ smsapp/app/src/main/java/com/ajinkya/smsapp/NotificationReceiver.java
@@ -0,0 +1,45 @@
+package com.ajinkya.smsapp;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class NotificationReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("1", "local", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(channel);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context.getApplicationContext(), 100, intent, PendingIntent.FLAG_MUTABLE);
+
+            Notification.Builder builder = new Notification.Builder(context, "1");
+            builder.setSmallIcon(R.drawable.baseline_add_alert_24).setAutoCancel(true).setContentIntent(pendingIntent).setContentTitle("Test Notification").setContentText("context is the test local notifications");
+
+            NotificationManagerCompat compat = NotificationManagerCompat.from(context);
+            if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            compat.notify(1, builder.build());
+        }
+    }
+}
